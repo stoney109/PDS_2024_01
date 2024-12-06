a13fb7c646e54178e1f538548d434a6c59a1fefb
stoney109 <stoney1og@gmail.com>
2024-12-06 14:08:58 +0900

    전체 데이터 나이 컬럼 추가, 농림 크롤링 코드 output 파일명 변경, 일부 파일 위치 이동

--- data_preprocessing/nonglim_birth_preprocessing.py
+++ data_preprocessing/nonglim_birth_preprocessing.py
@@ -15,6 +15,9 @@ def preprocess_birth(dataframe):
     # '(년생)'값 제거
     dataframe['출생연도'] = dataframe['출생연도'].str.replace(r'\(년생\)', '', regex=True)
 
+    # 특수기호 제거 (e.g. '.', '_')
+    dataframe['출생연도'] = dataframe['출생연도'].str.replace(r'[^0-9]', '', regex=True)
+
     # 두 자릿수 또는 한 자릿수 오류 처리
     def adjust_year_format(year):
         year = year.strip()  # 앞뒤 공백 제거
@@ -39,6 +42,9 @@ def preprocess_birth(dataframe):
             # 출생연도를 60일 전으로 계산하여 업데이트
             birth_date = registration_date - timedelta(days=60)
             dataframe.at[idx, '출생연도'] = birth_date.strftime('%Y.%m.%d')
+        elif len(row['출생연도']) > 4:
+            # 4자리 이상인 경우 앞의 4자리만 남김
+            dataframe.at[idx, '출생연도'] = row['출생연도'][:4]
 
     # '20nn' 형태만 남아있으면 '20nn.01.01'으로 수정
     year_only_mask = dataframe['출생연도'].str.match(r'^20\d{2}$')
@@ -49,6 +55,35 @@ def preprocess_birth(dataframe):
 
     return dataframe
 
+
+def add_age_column(dataframe):
+    """
+    '나이' 컬럼을 '출생연도'와 '접수일'을 기준으로 계산하여 추가하는 함수
+
+    Parameters:
+        dataframe (pd.DataFrame): 나이를 계산할 데이터프레임
+
+    Returns:
+        pd.DataFrame: 나이가 추가된 데이터프레임
+    """
+    def calculate_age(row):
+        try:
+            # 출생연도를 datetime 객체로 변환
+            birth_date = datetime.strptime(row['출생연도'], '%Y.%m')
+            # 접수일을 datetime 객체로 변환
+            registration_date = datetime.strptime(str(row['접수일']), '%Y%m%d')
+            # 나이를 연 단위로 계산
+            age = (registration_date - birth_date).days / 365.25  # 윤년 고려
+            if age < 0:  # 음수 값 처리
+                return 0  # 음수인 경우 0으로 처리 (총 117개의 입력 오류 데이터)
+            return round(age, 1)  # 소수점 첫째 자리까지 반올림
+        except Exception as e:
+            return None  # 변환 실패 시 NaN 처리
+
+    dataframe['나이'] = dataframe.apply(calculate_age, axis=1)
+    return dataframe
+
+
 # CSV 파일 경로
 input_csv_file = 'preprocessing_csv_files/nonglim_base_preprocessing.csv'
 output_csv_file = 'preprocessing_csv_files/nonglim_birth_preprocessing.csv'
@@ -58,6 +93,9 @@ data = pd.read_csv(input_csv_file)
 
 # 출생연도 전처리 함수 수행
 processed_data = preprocess_birth(data)
+
+# 나이 컬럼 추가
+processed_data = add_age_column(processed_data)
 
 # 최종 데이터프레임을 CSV로 저장
 processed_data.to_csv(output_csv_file, encoding='utf-8-sig', index=False)
